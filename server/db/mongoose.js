44d4bb6d6904b4722dc8247c75003be0499befac
ciprian87 <ciprianrobu@Ciprians-Mac-mini.local>
2017-08-23 21:57:42 -0500

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 // mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-let connection = mongoose.connect('mongodb://localhost:27017/TodoApp', {
+let connection = mongoose.connect('mongodb://admin:admin@ds113063.mlab.com:13063/nodetodosample' || 'mongodb://localhost:27017/TodoApp', {
     useMongoClient: true
   }).then(db => {
     console.log('MongoDB is connected')
